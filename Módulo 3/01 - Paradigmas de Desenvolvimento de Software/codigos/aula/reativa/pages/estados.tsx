ba4a5b0924f207b74780e8ffff6259b7caacea60
jpampereira <jpampereira@gmail.com>
2024-12-09 23:39:16 -0300

    Aula e prova 2 sobre Paradigmas de Desenvolvimento de Software

--- Módulo 3/01 - Paradigmas de Desenvolvimento de Software/codigos/aula/reativa/pages/estados.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "../styles/Home.module.css"; // Importa os estilos CSS
-
-function ContadorComMensagem() {
-  return <main className={styles.main}></main>;
-}
-
-export default ContadorComMensagem;
--- Módulo 3/01 - Paradigmas de Desenvolvimento de Software/codigos/referencial/declarativo/queries/calcular-media-livros.sql
+++
@@ -1,4 +0,0 @@
-SELECT generos.nome AS genero, AVG(livros.preco) AS preco_medio
-FROM livros
-JOIN generos ON livros.genero_id = generos.genero_id
-GROUP BY generos.nome;
